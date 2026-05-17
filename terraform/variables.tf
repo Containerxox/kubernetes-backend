0d7c07a9c60f95166016b5f2c76c7ef7698a1409
Containerxox <containerxox@gmail.com>
2026-05-17 23:26:49 +0900

    Update project files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,25 +5,7 @@ variable "aws_region" {
 }
 
 variable "project_name" {
-  description = "Project name prefix"
+  description = "Project name"
   type        = string
   default     = "devops-practice"
-}
-
-variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "db_username" {
-  description = "RDS master username"
-  type        = string
-  default     = "admin"
-}
-
-variable "db_password" {
-  description = "RDS master password"
-  type        = string
-  sensitive   = true
 }
